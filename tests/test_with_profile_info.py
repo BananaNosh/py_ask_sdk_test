5216ce25c3fb5c6c2841f056d6e8613ad138d6a5
Jonathan Loos <jonathan.loos@web.de>
2019-06-09 17:58:16 +0200

    Add with profile info

--- tests/test_with_profile_info.py
+++ tests/test_with_profile_info.py
@@ -11,6 +11,7 @@ def test_with_profile_info():
     alexa_test.test([
         TestItem(IntentRequestBuilder("DeiIntent", skill_settings).build(),
                  profile_info=ProfileInfo("Apollo", "Paian", "apollo@olympus.org", "003023520"),
+                 user_access_token="token",
                  expected_speech="Apollo potens est."
                  )
     ])
